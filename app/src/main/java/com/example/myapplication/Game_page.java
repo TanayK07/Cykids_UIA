a8c8b21fa3e6651fe9932f39d388534d89bcd0d0
TanayK07 <tkedia7@gmail.com>
2022-11-16 19:16:23 +0530

    Added Splash Activity

---
+++ app/src/main/java/com/example/myapplication/Game_page.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Game_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_page);
+    }
+}
+
+
---
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+     View btn1=(View)findViewById(R.id.rectangle_4);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashScreenActivity.this, Game_page.class));
+            }
+        });
+    }
+
+
+}
