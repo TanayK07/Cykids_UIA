c8c0bf0a75863f24219b3fa242efcf9143ebb0a9
Lahari <sailahari2002@gmail.com>
2022-11-23 09:11:09 +0530

    updated

--- app/src/main/java/com/example/endlessrunnergame/Ring.java
+++ app/src/main/java/com/example/endlessrunnergame/Ring.java
@@ -8,7 +8,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 
-import com.example.myapplication.R;
+
 
 import java.util.ArrayList;
 import java.util.Random;
